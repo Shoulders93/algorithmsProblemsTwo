8ec87ede13cb75e735da290837734928063130b1
Kory Attleson <kory.attleson@gmail.com>
2021-08-03 11:17:45 -0500

    Created scriptTwo.js file

---
+++ script.js
@@ -0,0 +1,17 @@
+// Determine if a number is a happy number
+// 13 is a happy number
+// 1^2 = 1, 3^2 = 9, 1 + 9 = 10; then 1^2 = 1, 0^2 = 0; 1 + 0 = 1
+
+// First I need to figure out how to square a number
+
+
+function happyNumber(number){
+    console.log(number);
+    let squaredNumber = (number*number);
+    console.log(squaredNumber);
+    for(i = 0; i < number.length; i++){
+        
+    }
+}
+
+happyNumber(prompt("Please enter a number"));
