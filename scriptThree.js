03d653ba3e61be25686a0b636a49399e2509e7a8
Kory Attleson <kory.attleson@gmail.com>
2021-08-03 14:07:55 -0500

    Started problem 3

---
+++ scriptThree.js
@@ -0,0 +1,13 @@
+// Fibonacci
+// series of numbers where each Fibonacci number is the sum of the two
+// preceding numbers. 1,1,2,3,5,8, etc
+// 1 +1 =2, 1 +2 = 3, 2 + 3 = 5 etc
+// write method that does the Fibonacci sequence starting at 1
+
+for(let i = 1; i <= 100; i++){
+    let firstNumber = 0;
+    let seconNumber = 1;
+    let newNumber;
+    newNumber =
+
+}
